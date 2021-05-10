216c6e273c38d006cd3bfca444ae4c39bea3f1ab
tabishshah139 <54994056+tabishshah139@users.noreply.github.com>
2021-05-10 23:17:09 +0500

    Update server.js

--- server.js
+++ server.js
@@ -866,7 +866,7 @@ theoleblanc03@gmail.com:Tanglewood29`;
       message.author.send("**Join Titanium Server** https://discord.gg/22CXbzku99");
      message.author.send({embed: {
         color: 3447003,
-        description: `**PUBG** \n \n ${random}`
+        description: `**NordVpn** \n \n ${random}`
       }});
       message.channel.send({embed: {
         color: 3066993,
@@ -877,32 +877,7 @@ theoleblanc03@gmail.com:Tanglewood29`;
 };
 });
 
-bot.on('message', message =>{
-    if (!message.guild) return;
-if (message.content === '+nordvpn'){
-    if (usedCommandRecently4.has(message.author.id)){
-        message.channel.send('``Wait 1 mintue before generating again``')
-    } else{
-        usedCommandRecently4.add(message.author.id);
-        setTimeout(() =>{
-            usedCommandRecently4.delete(message.author.id);
-        }, 60000)
-    var string = `Note: We have added Little Advertisement. You Can Get more than 50 Accounts by seeing advertisement 1 time. Watch Advertisement and Recieve Alot Accounts Thank You. :one: ( https://link-to.net/41622/nord ) 150x NordVPN Accounts list 1. :two: ( https://link-to.net/41622/norde2 ) 150x NordVPN Accounts list 2. :three: ( https://link-to.net/41622/norde3 ) 200x nordVPN Accounts list 3`;
-   var words = string.split('\n');
-    let random = words[Math.floor(Math.random()*words.length)];
-      message.author.send("**Join Tabish Gen Official CM** https://discord.gg/zGTR2Ra");
-     message.author.send({embed: {
-        color: 3447003,
-        description: `**NordVpn Account** \n \n ${random}`
-      }});
-      message.channel.send({embed: {
-        color: 3066993,
-        description: "NordVpn account sent on **DMs**! :white_check_mark:"
-   }});
-    };
-    
-};
-});
+
 
 bot.on('message', message =>{
   if (message.content === '+invite') {
